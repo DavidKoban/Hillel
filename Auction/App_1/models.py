e6ddcfe111baa0fe6edf6a6f5b1bcb5db3240b0c
DavidKoban <49913976+DavidKoban@users.noreply.github.com>
2022-05-16 21:11:39 +0300

    Update models.py

--- Auction/App_1/models.py
+++ Auction/App_1/models.py
@@ -41,11 +41,3 @@ class Photo(models.Model):
 
     def __str__(self):
         return f"{self.id}"
-
-    name = models.CharField(null=True, max_length=50, default="Ivan")
-    surname = models.CharField(null=True, max_length=50, default="Ivanov")
-    patronymic = models.CharField(null=True, max_length=50, default="Ivanovich")
-
-    def __str__(self):
-        return f"{self.id} {self.name}"
-
