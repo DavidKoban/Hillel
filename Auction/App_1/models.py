52ee2c24b0bf54965ec6f9a7b78b71609e92790e
DavidKoban <49913976+DavidKoban@users.noreply.github.com>
2022-05-16 21:14:54 +0300

    Update models.py

--- Auction/App_1/models.py
+++ Auction/App_1/models.py
@@ -13,7 +13,7 @@ class Auctioneer(models.Model):
     email = models.EmailField(null=False, default='owner@mail.com')
     birthday = models.DateField(null=False, default=timezone.now)
     # phone_number = PhoneField(blank=True, E164_only=False, null=False, default='+12223334444')
-
+    # Коммит нужный в дальнейшем это будет использовать, но возникла проблема, мы ее решим по мере разработки
     def __str__(self):
         return f"{self.id} {self.name}"
 
