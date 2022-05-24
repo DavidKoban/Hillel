47a31327f2eb562693030b61e87ad11eb5ad1c4d
Pavlo Shev4uk <95368807+Brokerok@users.noreply.github.com>
2022-05-24 20:52:50 +0300

    Update test_Auctioneer.py

--- Auction/Sloter/tests/test_Auctioneer.py
+++ Auction/Sloter/tests/test_Auctioneer.py
@@ -1,14 +1,14 @@
 import pytest
 from django.test import TestCase
-from App_1.models import Auctioneer
+from Sloter.models import User
 import unittest
 
 
-class test_Auctioneer(TestCase):
+class test_User(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Set up non-modified objects used by all test methods
-        Auctioneer.objects.create(name='Big',
+        User.objects.create(name='Big',
                                   surname='Bob',
                                   patronymic="Pob",
                                   password="1234",
@@ -17,6 +17,6 @@ class test_Auctioneer(TestCase):
                                   )
 
     def test_name(self):
-        auctioneer = Auctioneer.objects.first()
+        auctioneer = User.objects.first()
         field_label = auctioneer.name
         self.assertEqual(field_label, 'Big')
