6b6a5e00d4ba845f890a5ce475287d446b1fde15
Pavlo Shev4uk <95368807+Brokerok@users.noreply.github.com>
2022-05-24 20:49:24 +0300

    Update User.py

--- Auction/Sloter/models/User.py
+++ Auction/Sloter/models/User.py
@@ -12,7 +12,6 @@ class User(BasicModel, AbstractBaseUser, PermissionsMixin):
 
     surname = models.CharField(null=False, max_length=50, default="Ivanov")
     patronymic = models.CharField(null=True, max_length=50, default="Ivanovich")
-    password = models.CharField(null=False, max_length=50, default=' ')
     email = models.EmailField(null=False, default='owner@mail.com')
     birthday = models.DateField(null=False, default=timezone.now)
     # phone_number = PhoneField(blank=True, E164_only=False, null=False, default='+12223334444')
